d7f753b75c0d1e12414ce4c58cd9d37dffff5767
KellyKRaymond <kellyraymondis@gmail.com>
2022-01-27 14:02:33 -0500

    try again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const apiKey ='de804da427140e75c52bb7138208bf26'
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+      .then(res => res.json())
+      .then(res => console.log(res))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> this is a test </h1>
     </div>
   );
 }
