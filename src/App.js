0061fb40d2045bceb0c1a132f52b2fe95f9ea787
KellyKRaymond <kellyraymondis@gmail.com>
2022-01-30 22:39:49 -0500

    stopping here

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Clicked from './Components/clicked-video-page/clicked-video';
 import Header from './Components/header/header';
+import Footer from './Components/footer/footer';
 import Landing from './Components/landing-page/landing-page';
 import Search from './Components/search/search';
 import Video from './Components/video-card/video-card';
@@ -8,17 +9,30 @@ import React, { useState, useEffect } from 'react';
 
 // const apiKey = process.env.REACT_APP_API_KEY
 
+//  function App() {
+//   const [movieTitle, setMovieTitle] = useState([]);
+//   const [videoCard, setVideoCard] = useState([]);
+
+//   useEffect(() => {
+//     fetch(`https://api.themoviedb.org/3/movie/list?api_key=${apiKey}&language=en-US&query=${movieTitle}`)
+//       .then(res => res.json())
+//       .then(res => setMovieTitle(res.message))
+//   }, [])
+
 function App() {
   return (
     <div className="App">
       <Header />
       <div className='container'>
         <Clicked/>
+        <Footer/>
         <Landing/>
         <Search />
         <Video/>
+        {/* ({movieTitle.map(movieTitle => <movieTitle key= {movieTitle} movieTitle= {movieTitle}/>)}) */}
       </div>
     </div>
   );
 }
+ 
 export default App;
--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './footer.css'
 
-export default function Footer {
-    <div> 
+export default function Footer() {
+    return (
+    <div className="footer"> 
         <h1> whaddup world?????</h1>
     </div>
+    )
 }
