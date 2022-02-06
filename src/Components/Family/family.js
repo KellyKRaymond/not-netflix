cab428558fa62b41fa655f3a1db72da3eac8e91f
KellyKRaymond <kellyraymondis@gmail.com>
2022-02-05 23:40:39 -0500

    styling on single video page

--- src/Components/Family/family.js
+++ src/Components/Family/family.js
@@ -6,21 +6,17 @@ const apiKey = process.env.REACT_APP_API_KEY
 
 export default function Family(){
 const [family, setFamily] = useState([]);
-
+const [movies, setMovies] = useState([]);
     useEffect(() => {
-        fetch(`https://www.themoviedb.org/genre/10751-family/movie/?api_key=${apiKey}&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=10751`)
             .then(res => res.json())
             .then(res => (setFamily(res.results)))
     }, [])
-    
+    console.log(family)
     return(
         <>
         <h1> hi family friendly videos! </h1>
-        <div className="family-container"></div>
-            ({family.map(family => <VideoCard key={family.id} family={family} />)})
-            
+        ({movies.map(family => <VideoCard key={family.id} family={family} />)})
         </>
-
     )
-
 }
