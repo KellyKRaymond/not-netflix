37d9252344798e850ac419114105e8d740aa815f
KellyKRaymond <kellyraymondis@gmail.com>
2022-02-07 17:00:58 -0500

    family category

--- src/Components/Family/family.js
+++ src/Components/Family/family.js
@@ -4,19 +4,20 @@ import './family.css'
 
 const apiKey = process.env.REACT_APP_API_KEY
 
-export default function Family(){
-const [family, setFamily] = useState([]);
-const [movies, setMovies] = useState([]);
+export default function Family() {
+    const [movies, setMovies] = useState([]);
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=10751`)
             .then(res => res.json())
-            .then(res => (setFamily(res.results)))
+            .then(res => (setMovies(res.results)))
     }, [])
-    console.log(family)
-    return(
-        <>
-        <h1> hi family friendly videos! </h1>
-        ({movies.map(family => <VideoCard key={family.id} family={family} />)})
-        </>
+    console.log(movies)
+    return (
+        <div className="familyPage">
+            <h1> Family Friendly Movies! </h1>
+            <div className="videoRow">
+            ({movies.map(family => <VideoCard key={family.id} movie={family} />)})
+            </div>
+        </div>
     )
 }
