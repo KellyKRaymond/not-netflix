b827d2adb2ca2eb08494384ff54ee804a42491d8
KellyKRaymond <kellyraymondis@gmail.com>
2022-02-13 23:36:51 -0500

    tv shows

--- src/Components/tv-shows/tv-shows.js
+++ src/Components/tv-shows/tv-shows.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import VideoCard from "../video-card/video-card";
 import './tv-shows.css'
 
 const apiKey = process.env.REACT_APP_API_KEY
 
-export default function TvShows (){
+export default function TvShows() {
     const [movies, setTvShows] = useState([]);
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/tv/popular/?api_key=${apiKey}&language=en-US`)
@@ -16,7 +16,7 @@ export default function TvShows (){
         <div className="TvShowsPage">
             <h1> Tv Shows </h1>
             <div className="videoRow">
-            {movies.map(tvShow => <VideoCard key={tvShow.id} movie={tvShow} />)}
+                {movies.map(tvShow => <VideoCard key={tvShow.id} movie={tvShow} />)}
             </div>
         </div>
     )
